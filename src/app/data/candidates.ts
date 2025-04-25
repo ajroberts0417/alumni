a096b3c521654a63ba3accb55d2b5eeaa9984e64
ajroberts0417 <ajroberts0417@gmail.com>
2025-04-25 17:23:54 -0400

    add aaditya

--- src/app/data/candidates.ts
+++ src/app/data/candidates.ts
@@ -281,6 +281,20 @@ export const candidates: Candidate[] = [
         currentCompany: "Consulting: Sandbar, ESAI, HDR",
         headshot: "https://media.licdn.com/dms/image/v2/D4E03AQF97cnMl7Hw4A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725590217970?e=1750291200&v=beta&t=GGfN5A2oWny7DdTj7YrphEqyw1HIzsJbpCX4czKvVhA"
     },
+    {
+        name: "Aaditya Desai",
+        email: "aadityadesai09@gmail.com",
+        linkedin: "https://www.linkedin.com/in/aaditya-desai-2231851b6/",
+        twitter: "https://x.com/AadityaDesai9",
+        resume: "https://aadityadesai-resume.vercel.app/",
+        website: "https://aadityadesai.vercel.app/",
+        bio: "I’m Aaditya Desai and I'm a young software developer looking for an exceptional team to work with. I have a wide set of skills, easy to work with, and finished up a 3 month long, 60-hour week coding program. I'm working as an intern at Frame Media, but I’m currently looking for a software engineering internship where I can help other startups ship faster. Some of my work is here: https://aadityadesai.vercel.app/ I would like to work in defense tech companies like Anduril as a long-term goal of mine. I would like to work on their developmental weapons that they are creating for the U.S. military",
+        github: "https://github.com/adesai47",
+        headshot: "https://media.licdn.com/dms/image/v2/D5603AQF6sCVvAUbBkA/profile-displayphoto-shrink_400_400/B56ZUiLB6iGoAg-/0/1740035073216?e=1750896000&v=beta&t=jx0cQ03pB0hVHJPb2FlnLq4Ys3S2iQFf5m4jXLIYRdg",
+        cohort: "Fa24",
+        status: "open",
+        currentCompany: "Frame Media"
+    },
 ];
 
 // Sort candidates by status (open first, then hired/founded)
